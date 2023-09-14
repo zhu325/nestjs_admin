8d9bf9a7fb7412bae80d45496523a3ae0466c8a4
明凈日亱 <15668325325@126.com>
2023-09-14 23:59:12 +0800

    config🔧: 添加env配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import Joi from 'joi'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { DatabaseModule } from './database/database.module'
@@ -8,8 +10,32 @@ import { RolesModule } from './roles/roles.module'
 import { RolesService } from './roles/roles.service'
 import { UserModule } from './user/user.module'
 
+const envFilePath = `.env.${process.env.NODE_ENV || `development`}`
+
 @Module({
-  imports: [DatabaseModule, UserModule, LogsModule, RolesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      // load: [() => dotenv.config({ path: '.env' })],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+        DB_PORT: Joi.number().default(3306),
+        DB_HOST: Joi.alternatives().try(Joi.string().ip(), Joi.string().domain()),
+        DB_TYPE: Joi.string().valid('mysql', 'postgres'),
+        DB_DATABASE: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
+        LOG_ON: Joi.boolean(),
+        LOG_LEVEL: Joi.string(),
+      }),
+    }),
+    DatabaseModule,
+    UserModule,
+    LogsModule,
+    RolesModule,
+  ],
   controllers: [AppController, LogsController],
   providers: [AppService, RolesService],
 })
